2d4ec078969a8ea80b5928568002e843f6597c58
pshopov <99829036+pshopov@users.noreply.github.com>
2022-02-17 11:24:52 +0200

    Add files via upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+
+<html>
+   <head>
+      <title>Welcome </title>
+   </head>
+   <body>
+      <h4><?php echo $_SESSION['order']; ?></h4>
+      <h2><a href = "{{route('logout')}}">Sign Out</a></h2>
+   </body>
+</html>
